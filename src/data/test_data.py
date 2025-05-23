a24596fb25c3856ab4157ad8762714b9e7dd0c08
JakobOpresnik <jakob.opresnik@gmail.com>
2025-05-24 00:48:56 +0200

    Refactor pythons scripts and add some new yaml params - again

--- src/data/test_data.py
+++ src/data/test_data.py
@@ -7,6 +7,53 @@ import yaml
 from datetime import datetime
 
 
+def clean_data(reference_data, current_data):
+    # no need to compare dates
+    del reference_data["Date"]
+    del current_data["Date"]
+
+    # drop completely empty columns if they occur
+    reference_data.dropna(axis=1, how='all', inplace=True)
+    current_data.dropna(axis=1, how='all', inplace=True)
+
+
+def save_test_report(reports_file_path_template, station, result):
+    # create folder to save report if it doesn't exist yet
+    reports_path = reports_file_path_template.format(station=station)
+    if not os.path.exists(reports_path):
+        os.makedirs(reports_path)
+
+    # save report to HTML file
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    result.save_html(f"{reports_path}/data_testing_report_{station}_{timestamp}.html")
+
+
+def check_test_results(result, station, reference_path, current_path):
+    # check if report contains any tests and if all tests passed
+    all_tests_passed = True
+    result_dict = result.dict()
+
+    # print test results
+    if "tests" in result_dict:
+        for test in result_dict["tests"]:
+            if "status" in test and test["status"] != "SUCCESS":
+                all_tests_passed = False
+                if "name" in test:
+                    print(f"FAILED TEST: {test["name"].split(":")[-1]}")
+                if "description" in test:
+                    print(test["description"].split(":")[-1])
+                break
+                
+    if not all_tests_passed:
+        print(f"Data tests failed for station: {station}\n")
+    else:
+        print(f"Data tests passed for station: {station}")
+        # replace reference data with current data
+        os.remove(reference_path)
+        current = pd.read_csv(current_path)
+        current.to_csv(reference_path, index=False)
+
+
 def test_temperature_data():
     print("Current working directory:", os.getcwd())
 
@@ -31,13 +78,7 @@ def test_temperature_data():
 
         reference_data = pd.read_csv(reference_path)
 
-        # no need to compare dates
-        del reference_data["Date"]
-        del current_data["Date"]
-
-        # drop completely empty columns if they occur
-        reference_data.dropna(axis=1, how='all', inplace=True)
-        current_data.dropna(axis=1, how='all', inplace=True)
+        clean_data(reference_data, current_data)
 
         # define drift detection parameters
         drift_preset = DataDriftPreset(
@@ -56,35 +97,9 @@ def test_temperature_data():
         # run report on reference and current data
         result = report.run(reference_data=reference_data, current_data=current_data)
 
-        # create folder to save report if it doesn't exist yet
-        reports_path = reports_file_path_template.format(station=station)
-        if not os.path.exists(reports_path):
-            os.makedirs(reports_path)
+        save_test_report(reports_file_path_template, station, result)
+        check_test_results(result, station, reference_path, current_path)
 
-        # save report to HTML file
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        result.save_html(f"{reports_path}/data_testing_report_{station}_{timestamp}.html")
-
-
-        # check if report contains any tests and if all tests passed
-        all_tests_passed = True
-        result_dict = result.dict()
-
-        if "tests" in result_dict:
-            for test in result_dict["tests"]:
-                if "status" in test and test["status"] != "SUCCESS":
-                    all_tests_passed = False
-                    break
-                    
-        if not all_tests_passed:
-            print(f"Data tests failed for station: {station}")
-        else:
-            print(f"Data tests passed for station: {station}")
-            # replace reference data with current data
-            os.remove(reference_path)
-            current = pd.read_csv(current_path)
-            current.to_csv(reference_path, index=False)
- 
 
 if __name__ == "__main__":
     test_temperature_data()
