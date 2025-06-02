e8f333efca1f5eef2e029ff700d5e6a4b59d50bb
JakobOpresnik <jakob.opresnik@gmail.com>
2025-06-03 01:54:09 +0200

    Small f-string error fix

--- src/data/test_data.py
+++ src/data/test_data.py
@@ -41,7 +41,7 @@ def check_test_results(result, station, reference_path, current_path):
                 if "name" in test:
                     print(f"FAILED TEST: {test['name'].split(":")[0]}")
                 if "description" in test:
-                    print(test["description"].split(":")[-1])
+                    print(test["description"].split(':')[-1])
                 break
                 
     if not all_tests_passed:
