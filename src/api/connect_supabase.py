57d4dfc8ecea174eefc403a17af11057cd5f569c
JakobOpresnik <jakob.opresnik@gmail.com>
2025-06-10 08:20:56 +0200

    Update Dockerfile to use Railway variables for SUpabase connection

--- src/api/connect_supabase.py
+++ src/api/connect_supabase.py
@@ -1,8 +1,5 @@
 import os
-from dotenv import load_dotenv
 from supabase import create_client, Client
-
-load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
