3d31c2e850660ab6cae047112e65c2299f827fcc
JakobOpresnik <jakob.opresnik@gmail.com>
2025-06-11 23:05:37 +0200

    Fix endpoints

--- src/api/station.py
+++ src/api/station.py
@@ -5,22 +5,14 @@ from supabase import create_client, Client
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 
-print("SUPABASE_URL (station.py): ", url)
-
 supabase: Client = create_client(url, key)
 
 
 def fetch_stations():
     response = supabase.from_('station').select('*').execute()
-    if response.error:
-        raise Exception(f"Supabase error: {response.error.message}")
-    
     return response.data
 
 
 def fetch_station_by_name(station_name: str):
     response = supabase.from_('station').select('*').eq('name', station_name).single().execute()
-    if response.error:
-        raise Exception(f"Supabase error: {response.error.message}")
-
     return response.data
--- src/api/weather.py
+++ src/api/weather.py
@@ -1,14 +1,9 @@
 import os
 from supabase import create_client, Client
 
-from dotenv import load_dotenv
-load_dotenv()
-
 # handled by railway service variables
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-
-print("SUPABASE_URL (weather.py): ", url)
 
 supabase: Client = create_client(url, key)
 
